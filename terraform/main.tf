d2e90f3d2fe9ef64577a1180131ffb6b3580ebfb
Codeguruu <namananilgoyal@gmail.com>
2026-08-15 21:15:26 +0530

    feat: add GitHub Actions CI pipeline and Terraform provider configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = var.kubeconfig_path
+  config_path = fileexists(pathexpand(var.kubeconfig_path)) ? pathexpand(var.kubeconfig_path) : null
 }
 
 provider "helm" {
   kubernetes {
-    config_path = var.kubeconfig_path
+    config_path = fileexists(pathexpand(var.kubeconfig_path)) ? pathexpand(var.kubeconfig_path) : null
   }
 }
